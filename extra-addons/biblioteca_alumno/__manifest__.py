042eafaf28bccba0323b6ca52fa3289e03f95896
DE LA ROSA MUÑOZ <raul.de1@isepceu.es>
2025-12-12 18:35:20 +0100

    Añadir modulo Biblioteca Alumno

---
+++ extra-addons/biblioteca_alumno/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Biblioteca Alumno",
+    "version": "1.0",
+    "summary": "Módulo de práctica: gestión de libros",
+    "author": "Alumno DAM",
+    "category": "Teaching",
+    "depends": ["base"],
+    "data": [
+        "views/biblioteca_libro_views.xml",
+    ],
+    "installable": True,
+    "application": True, # Para que aparezca en el lanzador de Odoo como una app
+}
